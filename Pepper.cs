de3825bbf99e91e903290e6658de80f590df09de
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-07-18 00:53:28 +0700

    osu! : Add type parser for username resolution

--- Pepper.cs
+++ Pepper.cs
@@ -172,6 +172,7 @@ namespace Pepper
         protected override ValueTask AddTypeParsersAsync(CancellationToken cancellationToken = default)
         {
             Commands.AddTypeParser(RulesetTypeParser.Instance);
+            Commands.AddTypeParser(UsernameTypeParser.Instance);
             return base.AddTypeParsersAsync(cancellationToken);
         }
     }
