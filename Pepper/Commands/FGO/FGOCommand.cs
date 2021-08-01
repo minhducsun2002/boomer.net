21f0b0a82b1f0cf69542564d66bd311fc2a716a5
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-08-01 19:49:07 +0700

    F/GO : Apply prefix category for all commands under the category

--- Pepper/Commands/FGO/FGOCommand.cs
+++ Pepper/Commands/FGO/FGOCommand.cs
@@ -6,6 +6,7 @@ using Pepper.Structures.Commands;
 namespace Pepper.Commands.FGO
 {
     [Category("FGO")]
+    [PrefixCategory("fgo")]
     public abstract class FGOCommand : Command
     {
         protected readonly MasterDataService MasterDataService;
