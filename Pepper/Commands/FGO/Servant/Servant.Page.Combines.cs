6ded1709490aff1dccd80a90b3ed78fbdd90af81
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-08-24 11:19:09 +0700

    F/GO : Refactor servant view to use selection-based paged view

---
+++ Pepper/Commands/FGO/Servant/Servant.Page.Combines.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Disqord;
+using Disqord.Extensions.Interactivity.Menus.Paged;
+using Pepper.Structures.External.FGO.Entities;
+
+namespace Pepper.Commands.FGO
+{
+    public partial class Servant
+    {
+        private (LocalSelectionComponentOption, Page)? AscensionsPage(BaseServant servant, ServantLimits limits, IReadOnlyDictionary<int, string> itemNames)
+        {
+            if (limits.AscensionCombine.Length == 0) return null;
+            
+            var page = servant.BaseEmbed()
+                .WithDescription(limits.AscensionCombine.Length == 0 ? "No materials needed." : "")
+                .WithFields(
+                    limits.AscensionCombine.Select((limit, index) => new LocalEmbedField
+                    {
+                        Name = $"Stage {index + 1} - {limit.QP.ToString("n0", CultureInfo.InvariantCulture)} QP",
+                        Value = string.Join(
+                            '\n',
+                            limit.ItemIds.Zip(limit.ItemNums)
+                                .Select(tuple => $"- **{tuple.Second}**x **{itemNames[tuple.First]}**")
+                        )
+                    }));
+
+            return (new LocalSelectionComponentOption { Label = "Ascension materials" }, new Page().WithEmbeds(page));
+        }
+        
+        private (LocalSelectionComponentOption, Page)? SkillLimitsPage(BaseServant servant, ServantLimits limits, IReadOnlyDictionary<int, string> itemNames)
+        {
+            if (limits.SkillCombine.Length == 0) return null;
+            
+            var page = servant.BaseEmbed()
+                .WithDescription(limits.SkillCombine.Length == 0 ? "No materials needed." : "")
+                .WithFields(
+                    limits.SkillCombine.Select((limit, index) => new LocalEmbedField
+                    {
+                        Name = $"Stage {index + 2} - {limit.QP.ToString("n0", CultureInfo.InvariantCulture)} QP",
+                        Value = string.Join(
+                            '\n',
+                            limit.ItemIds.Zip(limit.ItemNums)
+                                .Select(tuple => $"- **{tuple.Second}**x **{itemNames[tuple.First]}**")
+                        )
+                    }));
+            
+            return (new LocalSelectionComponentOption { Label = "Skill materials" }, new Page().WithEmbeds(page));
+        }
+    }
+}
--- Pepper/Commands/FGO/Servant/Servant.cs
+++ Pepper/Commands/FGO/Servant/Servant.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Disqord;
 using Disqord.Bot;
 using Disqord.Extensions.Interactivity.Menus;
 using FgoExportedConstants;
 using Qmmands;
 using MongoDB.Driver;
 using Pepper.Services.FGO;
+using Pepper.Structures;
 using Pepper.Structures.Commands;
 using Pepper.Structures.External.FGO;
 using Pepper.Structures.External.FGO.Entities;
@@ -16,7 +18,7 @@ using Pepper.Structures.External.FGO.Renderer;
 
 namespace Pepper.Commands.FGO
 {
-    public class Servant : ServantCommand
+    public partial class Servant : ServantCommand
     {
         public Servant(MasterDataService m, TraitService t, ItemNamingService i, ServantNamingService n) : base(m, t, i, n) {}
         
@@ -48,85 +50,22 @@ namespace Pepper.Commands.FGO
                         return na.GetItemName(itemId) ?? jp.GetItemName(itemId)!;
                     }
                 );
-            
-            // preparing the CE
-            (string, int, int, IEnumerable<string>)? ceDetails = null;
-            var bondCESkill = jp.MstSkill.FindSync(Builders<MstSkill>.Filter.Eq("actIndividuality", servant.ID)).FirstOrDefault();
-            if (bondCESkill != default)
-            {
-                var skillId = bondCESkill.ID;
-                var skillMapping = jp.MstSvtSkill.FindSync(Builders<MstSvtSkill>.Filter.Eq("skillId", skillId)).FirstOrDefault();
-                if (skillMapping != default)
-                {
-                    var svtQuery = Builders<MstSvt>.Filter.Eq("id", skillMapping.SvtId);
-                    var ceSvt = jp.MstSvt.FindSync(svtQuery).First();
-                    var naName = na.MstSvt.FindSync(svtQuery).FirstOrDefault()?.Name;
-                    var mstSkill = jp.MstSkill.FindSync(Builders<MstSkill>.Filter.Eq("id", skillId)).First();
-                    var (skill, referencedSkills) = new SkillRenderer(mstSkill, jp).Prepare(TraitService);
-                    ceDetails = (naName ?? ceSvt.Name, ceSvt.CollectionNo, ceSvt.ID, skill);
-                }
-            }
-            
-            // preparing passive skills
-            var passives = servant.ServantEntity.ClassPassive.Select(skillId => jp.GetSkillById(skillId))
-                .Select(skill =>
-                {
-                    // overwrite with NA name
-                    try { skill.MstSkill.Name = na.GetSkillById(skill.MstSkill.ID).MstSkill.Name; } catch { /* ignore */ }
-                    
-                    var (baseSkill, referencedSkills) = new SkillRenderer(skill.MstSkill, jp, skill).Prepare(TraitService);
-                    return (skill, baseSkill, referencedSkills);
-                }).ToList()!;
 
-            return View(
-                new ServantView(
+            var pages = new[]
+            {
+                GeneralPage(
                     servant,
-                    GetNPGain(servant.ID),
-                    jp.MstSvtCard.FindSync(Builders<MstSvtCard>.Filter.Eq("svtId", servant.ID)).ToList(),
-                    limits,
-                    TraitService,
-                    itemNames,
-                    passives,
-                    ceDetails,
-                    Context.Message.Id
-                )
-            );
-        }
-
-        private MstTreasureDeviceLv GetNPGain(int svtId)
-        {
-            var jp = MasterDataService.Connections[Region.JP];
-            var mapping = jp.MstSvtTreasureDevice.FindSync(
-                Builders<MstSvtTreasureDevice>.Filter.And(
-                    Builders<MstSvtTreasureDevice>.Filter.Eq("svtId", svtId),
-                    Builders<MstSvtTreasureDevice>.Filter.Eq("num", 1)
+                    jp.MstSvtCard.FindSync(Builders<MstSvtCard>.Filter.Eq("svtId", servant.ID)).ToList()
                 ),
-                new FindOptions<MstSvtTreasureDevice> {Limit = 1}
-            ).First()!;
-            return jp.MstTreasureDeviceLv.FindSync(
-                Builders<MstTreasureDeviceLv>.Filter.Eq("treaureDeviceId", mapping.TreasureDeviceId),
-                new FindOptions<MstTreasureDeviceLv> {Limit = 1}
-            ).First()!;
-        }
-        
-        public static readonly Dictionary<BattleCommand.TYPE, Tuple<int, int[], string>> DefaultCardTypes = new()
-        {
-            { BattleCommand.TYPE.ARTS, new Tuple<int, int[], string>(0, Array.Empty<int>(), "Arts  ") },
-            { BattleCommand.TYPE.BUSTER, new Tuple<int, int[], string>(0, Array.Empty<int>(), "Buster") },
-            { BattleCommand.TYPE.QUICK, new Tuple<int, int[], string>(0, Array.Empty<int>(), "Quick ") },
-            { BattleCommand.TYPE.ADDATTACK, new Tuple<int, int[], string>(0, Array.Empty<int>(), "Extra ") }
-        };
-        private static Dictionary<BattleCommand.TYPE, Tuple<int, int[], string>> GetCardStatistics(IReadOnlyCollection<MstSvtCard> cards)
-        {
-            var ret = new Dictionary<BattleCommand.TYPE, Tuple<int, int[], string>>(DefaultCardTypes);
-            foreach (var (type, record) in ret)
-            {
-                var count = cards.Aggregate(0, (acc, card) => acc + (card.CardId == (int) type ? 1 : 0));
-                var damage = cards.First(card => card.CardId == (int) type).NormalDamage;
-                ret[type] = new Tuple<int, int[], string>(count, damage, record.Item3);
+                AscensionsPage(servant, limits, itemNames),
+                SkillLimitsPage(servant, limits, itemNames),
+                PassivesPage(servant)
             }
+                .Where(entry => entry.HasValue)
+                .Select(entry => entry!.Value)
+                .ToArray();
 
-            return ret;
+            return View(new SelectionPagedView(pages, new LocalMessage().WithReply(Context.Message.Id)));
         }
     }
 }
