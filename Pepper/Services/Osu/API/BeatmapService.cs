2d423017515f71b3673c19368c1cbf2f3f5eb7eb
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-09-05 02:19:20 +0700

    osu! : Stop reusing WorkingBeatmap instances
    They seem to persist difficulty attributes, resulting in duplicate mod applications when used more than once.

--- Pepper/Services/Osu/API/BeatmapService.cs
+++ Pepper/Services/Osu/API/BeatmapService.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BitFaster.Caching.Lru;
@@ -12,25 +13,30 @@ namespace Pepper.Services.Osu.API
     internal class BeatmapCache
     {
         private static readonly HttpClient HttpClient = new();
-        private readonly FastConcurrentLru<long, WorkingBeatmap> cache = new(200);
+        private readonly FastConcurrentLru<long, byte[]> cache = new(200);
 
-        public async Task<WorkingBeatmap> GetBeatmap(long beatmapId)
+        private WorkingBeatmap decodeBeatmapFile(byte[] file)
         {
-            if (cache.TryGet(beatmapId, out var @return)) return @return;
-
-            await using var stream = await HttpClient.GetStreamAsync($"https://osu.ppy.sh/osu/{beatmapId}");
+            using var stream = new MemoryStream(file);
             using var streamReader = new LineBufferedReader(stream);
             var workingBeatmap = new WorkingBeatmap(Decoder.GetDecoder<Beatmap>(streamReader).Decode(streamReader));
             if (workingBeatmap.BeatmapInfo.Length.Equals(default))
                 workingBeatmap.BeatmapInfo.Length = workingBeatmap.Beatmap.HitObjects[^1].StartTime;
             workingBeatmap.BeatmapInfo.Ruleset =
                 RulesetTypeParser.SupportedRulesets[workingBeatmap.BeatmapInfo.RulesetID].RulesetInfo;
-            
-            cache.AddOrUpdate(beatmapId, workingBeatmap);
-            
-            return workingBeatmap;
 
+            return workingBeatmap;
+        }
+        
+        public async Task<WorkingBeatmap> GetBeatmap(long beatmapId)
+        {
+            if (cache.TryGet(beatmapId, out var @return)) return decodeBeatmapFile(@return);
+
+            var file = await HttpClient.GetByteArrayAsync($"https://osu.ppy.sh/osu/{beatmapId}");
+
+            cache.AddOrUpdate(beatmapId, file);
             
+            return decodeBeatmapFile(file);
         }
     }
 }
