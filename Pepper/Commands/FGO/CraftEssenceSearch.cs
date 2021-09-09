348b54e996298dbd358ecf73a81cf0b757f2a7b9
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-09-09 17:32:57 +0700

    F/GO : Implement CE search command

---
+++ Pepper/Commands/FGO/CraftEssenceSearch.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Disqord.Bot;
+using Disqord.Extensions.Interactivity.Menus.Paged;
+using Humanizer;
+using Pepper.Services.FGO;
+using Pepper.Structures;
+using Pepper.Structures.External.FGO;
+using Pepper.Structures.External.FGO.Entities;
+using Qmmands;
+using LocalEmbed = Disqord.LocalEmbed;
+using LocalEmbedField = Disqord.LocalEmbedField;
+using PagedView = Pepper.Structures.PagedView;
+
+namespace Pepper.Commands.FGO
+{
+    public class CraftEssenceSearch : FGOCommand
+    {
+        private readonly CraftEssenceNamingService ceNamingService;
+        public CraftEssenceSearch(CraftEssenceNamingService craftEssenceNamingService)
+            => ceNamingService = craftEssenceNamingService;
+        
+        public async Task<DiscordCommandResult> Exec(
+            [Description("A CE name, ID, or collectionNo.")] string query
+        )
+        {
+            if (string.IsNullOrWhiteSpace(query)) return Reply("Please specify a query :frowning:");
+            var searchResults = ceNamingService.Namings.FuzzySearch(query);
+            var isOwner = await Context.Bot.IsOwnerAsync(Context.Author.Id);
+            var pageProvider = new ArrayPageProvider<NamedKeyedEntitySearchResult<int, string>>(
+                searchResults,
+                (_, segment) => new Page().WithEmbeds(
+                    new LocalEmbed
+                    {
+                        Title = $"Search results for `{query}`",
+                        Description = string.Join(
+                            "\n ",
+                            segment.Select(result =>
+                                $"{ceNamingService.CEIdToCollectionNo[result.Key]}. **{result.Name}**"
+                                + (isOwner ? $" [{result.Score:0.#}]" : "")
+                            )
+                        )
+                    }),
+                Math.Min(15, searchResults.Length)
+            );
+            
+            return View(new PagedView(pageProvider));
+        }
+    }
+}
