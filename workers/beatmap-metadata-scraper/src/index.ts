a74c2f3f3e126cb11bec6c7de18dca226318f55d
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2021-12-24 12:21:00 +0700

    osu! : In case scraping beatmap data fails, fallback to APIv2

--- workers/beatmap-metadata-scraper/src/index.ts
+++ workers/beatmap-metadata-scraper/src/index.ts
@@ -2,7 +2,34 @@ addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 
+declare var token : KVNamespace;
 type HTMLRewriterElementContentHandler = Parameters<HTMLRewriter['on']>[1];
+
+async function getToken() {
+    let ret = await token.get('token');
+    if (!ret) {
+        let id = await token.get('id');
+        let secret = await token.get('secret');
+        let response = await fetch('https://osu.ppy.sh/oauth/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                client_id: +id!,
+                client_secret: secret,
+                grant_type: 'client_credentials',
+                scope: 'public'
+            })
+        });
+
+        let body: { expires_in: number, access_token: string } = await response.json();
+        await token.put('token', body.access_token, {
+            expirationTtl: body.expires_in - 100
+        });
+    }
+    return await token.get('token');
+}
 
 async function handleRequest(req : Request) {
     let url = new URL(req.url);
@@ -15,10 +42,31 @@ async function handleRequest(req : Request) {
             status: 400
         });
     }
-
     let o = await Promise.all(
         ids.map(async id => {
             let s = await fetch(`https://osu.ppy.sh/${isSet ? 'beatmapsets' : 'beatmaps'}/${id}`)
+
+            if (s.status === 429) {
+                let token = await getToken();
+                let setid = id;
+                if (!isSet) {
+                    setid = await fetch(`https://storage.ripple.moe/api/b/${id}`)
+                        .then(r => r.json()).then((r: any) => r?.ParentSetID).catch(() => null);
+                    if (!setid) {
+                        setid = await fetch(`https://osu.ppy.sh/api/v2/beatmaps/${id}`, {
+                            headers: {
+                                'Authorization': `Bearer ${token}`
+                            }
+                        }).then(r => r.json()).then((r: any) => r.id);
+                    }
+                }
+
+                return await fetch(`https://osu.ppy.sh/api/v2/beatmapsets/${id}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                }).then(res => res.json())
+            }
 
             let json = '';
             class Handler implements HTMLRewriterElementContentHandler {
@@ -28,11 +76,14 @@ async function handleRequest(req : Request) {
             }
 
             await new HTMLRewriter().on('#json-beatmapset', new Handler()).transform(s).text();
-            return json;
+            if (s.status != 200) {
+                throw new Error(`Fetching id ${id} failed : status was ${s.status}`);
+            }
+            return JSON.parse(json);
         })
     )
 
-    return new Response(JSON.stringify(JSON.parse(`[${o.join(',')}]`)), {
+    return new Response(JSON.stringify(o), {
         headers: {
             'Content-Type': 'application/json'
         }
