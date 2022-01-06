ea8b6240207f7242f421a0f45fab06a68f983cec
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-01-06 10:30:06 +0700

    osu! : Update user data scraping logic

--- Pepper.Commons.Osu/APIClients/Default/DefaultOsuAPIClient.User.cs
+++ Pepper.Commons.Osu/APIClients/Default/DefaultOsuAPIClient.User.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using BitFaster.Caching.Lru;
 using HtmlAgilityPack;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using osu.Game.Rulesets;
 using Pepper.Commons.Osu.API;
 using Color = System.Drawing.Color;
@@ -18,12 +19,12 @@ namespace Pepper.Commons.Osu.APIClients.Default
         {
             var rulesetName = rulesetInfo.ShortName;
             var html = await HttpClient.GetStringAsync($"https://osu.ppy.sh/users/{HttpUtility.UrlPathEncode(username)}/{rulesetName}");
+
             var doc = new HtmlDocument(); doc.LoadHtml(html);
-            var user = JsonConvert.DeserializeObject<APIUser>(
-                doc.GetElementbyId("json-user").InnerText,
-                new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }
-            )!;
-            return user;
+            var raw = doc.DocumentNode.Descendants("div").First(node => node.HasClass("js-react--profile-page"))!;
+            var data = raw.GetAttributeValue("data-initial-data", "")?.Replace("&quot;", "\"");
+            var parsed = JObject.Parse(data!);
+            return parsed["user"]!.ToObject<APIUser>()!;
         }
     }
 }
