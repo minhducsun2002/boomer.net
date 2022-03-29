40e74523db7160cd602d7a5e3f8d5f3905230bc5
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-03-29 20:40:49 +0700

    osu! : Remove Ripple API client's dependency on OsuSharp

--- Pepper.Commons.Osu/APIClients/Ripple/RippleOsuAPIClient.cs
+++ Pepper.Commons.Osu/APIClients/Ripple/RippleOsuAPIClient.cs
@@ -1,5 +1,4 @@
 using System.Net.Http;
-using OsuSharp;
 
 namespace Pepper.Commons.Osu.APIClients.Ripple
 {
@@ -7,18 +6,15 @@ namespace Pepper.Commons.Osu.APIClients.Ripple
     public partial class RippleOsuAPIClient : APIClient
     {
         private readonly OsuRestClient restClient;
-        private readonly OsuClient osuSharpClient;
 
         /// <summary>
         /// Create an <see cref="APIClient"/> for Ripple server.
         /// </summary>
         /// <param name="httpClient">Self-explanatory.</param>
         /// <param name="restClient">OAuth2 REST client implementation.</param>
-        /// <param name="osuSharpClient">Self-explanatory.</param>
-        public RippleOsuAPIClient(HttpClient httpClient, OsuRestClient restClient, OsuClient osuSharpClient) : base(httpClient)
+        public RippleOsuAPIClient(HttpClient httpClient, OsuRestClient restClient) : base(httpClient)
         {
             this.restClient = restClient;
-            this.osuSharpClient = osuSharpClient;
         }
     }
 }
