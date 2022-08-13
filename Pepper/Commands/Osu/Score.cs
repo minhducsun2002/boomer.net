cdebe8c555a2c922d8cf21410eea396474bcd26b
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-08-14 00:26:01 +0700

    osu! : Make username **actually** required for viewing scores

--- Pepper/Commands/Osu/Score.cs
+++ Pepper/Commands/Osu/Score.cs
@@ -28,84 +28,74 @@ namespace Pepper.Commands.Osu
         public async Task<IDiscordCommandResult> BeatmapBased(
             [Description("A score URL, a beatmap URL, or a beatmap ID.")] IBeatmapResolvable beatmapResolvable,
             [Flag("-")][Description("Game server to check. Default to osu! official servers.")] GameServer server,
-            [Remainder][Description("Username to check. Default to your username, if set.")] Username? username = null
+            [Remainder][Description("Username to check. Default to your username, if set.")] Username username
         )
         {
             var mapId = beatmapResolvable.BeatmapId;
-            if (username != null)
+            var apiClient = APIClientStore.GetClient(server);
+            var map = await APIClientStore.GetClient(GameServer.Osu).GetBeatmap(mapId);
+            var ruleset = map.GetDefaultRuleset();
+            SetBeatmapContext(mapId);
+
+            var user = await apiClient.GetUser(username.GetUsername(server)!, ruleset.RulesetInfo);
+            var scores = await apiClient.GetUserBeatmapScores(
+                user.Id,
+                mapId,
+                ruleset.RulesetInfo
+            );
+
+            if (scores.Length == 0)
             {
-                var apiClient = APIClientStore.GetClient(server);
-                var map = await APIClientStore.GetClient(GameServer.Osu).GetBeatmap(mapId);
-                var ruleset = map.GetDefaultRuleset();
-                SetBeatmapContext(mapId);
+                return Reply($"No score found on that beatmap for user `{user.Username}`.");
+            }
 
-                var user = await apiClient.GetUser(username.GetUsername(server)!, ruleset.RulesetInfo);
-                var scores = await apiClient.GetUserBeatmapScores(
-                    user.Id,
-                    mapId,
-                    ruleset.RulesetInfo
-                );
 
-                if (scores.Length == 0)
+            var difficulty = map.CalculateDifficulty(ruleset.RulesetInfo.OnlineID);
+            HitStatisticsSynthesizer? synthesizer = null;
+
+            return Reply(new LocalEmbed
+            {
+                Author = SerializeAuthorBuilder(user),
+                Title = $"**{map.Beatmap.Metadata.Artist}** - **{map.Beatmap.Metadata.Title}** [{map.BeatmapInfo.DifficultyName}]",
+                Url = map.GetOnlineUrl(),
+                Description = $"{SerializeBeatmapStats(map.BeatmapInfo, null, difficulty, map.Beatmap.ControlPointInfo)}\n\n"
+                              + string.Join("\n\n", scores.Select(score =>
+                              {
+                                  var localPP = false;
+                                  var mods = ModParserService.ResolveMods(ruleset, score.Mods);
+                                  var pp = score.PP;
+                                  if (!pp.HasValue)
+                                  {
+                                      try
+                                      {
+                                          synthesizer ??= new HitStatisticsSynthesizer(map.Beatmap.HitObjects.Count);
+                                          var scoreInfo = new ScoreInfo
+                                          {
+                                              Mods = mods,
+                                              MaxCombo = difficulty.MaxCombo,
+                                              Accuracy = score.Accuracy,
+                                              Statistics = synthesizer.Synthesize(ruleset, score.Accuracy)
+                                          };
+                                          pp = (float) map.CalculatePerformance(scoreInfo);
+                                          localPP = true;
+                                      }
+                                      catch { /* ignore */ }
+                                  }
+
+                                  return
+                                      $"[**{score.Rank}**] **{pp}**pp{(localPP ? " (?)" : "")} (**{score.MaxCombo}**x | **{score.Accuracy:F3}**%)"
+                                      + $" {(score.Perfect ? "(FC)" : "")}"
+                                      + (mods.Length != 0 ? $"+**{string.Join("", mods.Select(mod => mod.Acronym))}**" : "")
+                                      + $"\n" + SerializeHitStats(score.Statistics, Rulesets[score.RulesetID].RulesetInfo)
+                                      + $" @ **{SerializeTimestamp(score.Date, false)}**"
+                                      + $"\n[**Score link**](https://osu.ppy.sh/scores/{ruleset.ShortName}/{score.OnlineID})";
+                              })),
+                Timestamp = DateTimeOffset.Now,
+                Footer = new LocalEmbedFooter
                 {
-                    return Reply($"No score found on that beatmap for user `{user.Username}`.");
+                    Text = "All times are in UTC"
                 }
-
-
-                var difficulty = map.CalculateDifficulty(ruleset.RulesetInfo.OnlineID);
-                HitStatisticsSynthesizer? synthesizer = null;
-
-                return Reply(new LocalEmbed
-                {
-                    Author = SerializeAuthorBuilder(user),
-                    Title = $"**{map.Beatmap.Metadata.Artist}** - **{map.Beatmap.Metadata.Title}** [{map.BeatmapInfo.DifficultyName}]",
-                    Url = map.GetOnlineUrl(),
-                    Description = $"{SerializeBeatmapStats(map.BeatmapInfo, null, difficulty, map.Beatmap.ControlPointInfo)}\n\n"
-                        + string.Join("\n\n", scores.Select(score =>
-                        {
-                            var localPP = false;
-                            var mods = ModParserService.ResolveMods(ruleset, score.Mods);
-                            var pp = score.PP;
-                            if (!pp.HasValue)
-                            {
-                                try
-                                {
-                                    synthesizer ??= new HitStatisticsSynthesizer(map.Beatmap.HitObjects.Count);
-                                    var scoreInfo = new ScoreInfo
-                                    {
-                                        Mods = mods,
-                                        MaxCombo = difficulty.MaxCombo,
-                                        Accuracy = score.Accuracy,
-                                        Statistics = synthesizer.Synthesize(ruleset, score.Accuracy)
-                                    };
-                                    pp = (float) map.CalculatePerformance(scoreInfo);
-                                    localPP = true;
-                                }
-                                catch { /* ignore */ }
-                            }
-
-                            return
-                                $"[**{score.Rank}**] **{pp}**pp{(localPP ? " (?)" : "")} (**{score.MaxCombo}**x | **{score.Accuracy:F3}**%)"
-                                + $" {(score.Perfect ? "(FC)" : "")}"
-                                + (mods.Length != 0 ? $"+**{string.Join("", mods.Select(mod => mod.Acronym))}**" : "")
-                                + $"\n" + SerializeHitStats(score.Statistics, Rulesets[score.RulesetID].RulesetInfo)
-                                + $" @ **{SerializeTimestamp(score.Date, false)}**"
-                                + $"\n[**Score link**](https://osu.ppy.sh/scores/{ruleset.ShortName}/{score.OnlineID})";
-                        })),
-                    Timestamp = DateTimeOffset.Now,
-                    Footer = new LocalEmbedFooter
-                    {
-                        Text = "All times are in UTC"
-                    }
-                });
-            }
-
-            if (username == null)
-            {
-                throw new ArgumentException("No username is passed!");
-            }
-
-            throw new ArgumentException("A valid beatmap-resolvable must be passed!");
+            });
         }
 
         [TextCommand("sc", "score", "scores", "c", "check")]
@@ -113,7 +103,7 @@ namespace Pepper.Commands.Osu
         [OverloadPriority(1)]
         public async Task<IDiscordCommandResult> UserOnly(
             [Flag("-")][Description("Game server to check. Default to osu! official servers.")] GameServer server,
-            [Remainder][Description("Username to check. Default to your username, if set.")] Username? username = null
+            [Remainder][Description("Username to check. Default to your username, if set.")] Username username
         )
         {
             var beatmap = BeatmapContext.GetBeatmap(Context.ChannelId.ToString());
@@ -126,27 +116,27 @@ namespace Pepper.Commands.Osu
             return await BeatmapBased(new BeatmapResolvable(beatmap.Value), server, username);
         }
 
-        [TextCommand("sc", "score", "scores", "c", "check")]
-        [Description("View/list scores on a certain map (osu! official servers only)")]
-        [OverloadPriority(0)]
-        public async Task<IDiscordCommandResult> ScoreLink([Description("A link to a score.")] string link)
-        {
-            if (URLParser.CheckScoreUrl(link, out var scoreLink))
-            {
-                var (mode, id) = scoreLink;
-                var sc = await APIClientStore.GetClient(GameServer.Osu).GetScore(
-                    id,
-                    Rulesets
-                        .First(rulesetCheck => string.Equals(rulesetCheck.ShortName, mode,
-                            StringComparison.InvariantCultureIgnoreCase))
-                        .RulesetInfo
-                );
-                return await SingleScore(sc);
-            }
-
-            return Reply(string.IsNullOrWhiteSpace(link)
-                ? "Couldn't determine a beatmap to use in this channel. It's likely that I'm recently restarted."
-                : "Your score link doesn't look valid.");
-        }
+        // [TextCommand("sc", "score", "scores", "c", "check")]
+        // [Description("View/list scores on a certain map (osu! official servers only)")]
+        // [OverloadPriority(0)]
+        // public async Task<IDiscordCommandResult> ScoreLink([Description("A link to a score.")] string link)
+        // {
+        //     if (URLParser.CheckScoreUrl(link, out var scoreLink))
+        //     {
+        //         var (mode, id) = scoreLink;
+        //         var sc = await APIClientStore.GetClient(GameServer.Osu).GetScore(
+        //             id,
+        //             Rulesets
+        //                 .First(rulesetCheck => string.Equals(rulesetCheck.ShortName, mode,
+        //                     StringComparison.InvariantCultureIgnoreCase))
+        //                 .RulesetInfo
+        //         );
+        //         return await SingleScore(sc);
+        //     }
+        //
+        //     return Reply(string.IsNullOrWhiteSpace(link)
+        //         ? "Couldn't determine a beatmap to use in this channel. It's likely that I'm recently restarted."
+        //         : "Your score link doesn't look valid.");
+        // }
     }
 }
