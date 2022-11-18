85282fb6573c6a04b417b2d63e65443528dde052
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-11-18 14:42:20 +0700

    Remove support for custom command context

--- Pepper/Structures/Command.cs
+++ Pepper/Structures/Command.cs
@@ -2,7 +2,5 @@ using Disqord.Bot.Commands.Text;
 
 namespace Pepper.Structures
 {
-    public abstract class BaseCommand<T> : DiscordTextModuleBase where T : IDiscordTextCommandContext { }
-
-    public abstract class Command : BaseCommand<IDiscordTextCommandContext> { }
+    public abstract class Command : DiscordTextModuleBase { }
 }
