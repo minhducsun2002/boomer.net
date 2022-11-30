e83a1aab45edd7655bd5c3a1c5f3482eca876667
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-11-30 20:52:10 +0700

    maimaiDX : Parse user play count while ignoring commas

--- Pepper.Commons.Maimai/HtmlParsers/PlayerDataParser.cs
+++ Pepper.Commons.Maimai/HtmlParsers/PlayerDataParser.cs
@@ -16,7 +16,7 @@ namespace Pepper.Commons.Maimai.HtmlParsers
             var name = main.QuerySelector(".name_block");
             var avatar = main.QuerySelector(".w_112.f_l");
             var playCountText = main.QuerySelector(".m_5.m_t_10.t_r.f_12");
-            var playCount = FastIntParse(GetIntFromString(playCountText.InnerText));
+            var playCount = FastIntParseIgnoreCommaAndSpace(GetIntFromString(playCountText.InnerText));
             var danImageLink = main.QuerySelector(".h_35.f_l").GetAttributeValue("src", "");
             var danLevel = GetTwoDigitsData("rank_", danImageLink)!.Value;
             var seasonClassImageLink = main.QuerySelector(".p_l_10.h_35.f_l").GetAttributeValue("src", "");
