137a6f9b74c71d24f34d52b2b340d21cd5a144bc
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2022-12-03 17:03:16 +0700

    maimaiDX : Improve missing cookie message

--- Pepper.Commons.Maimai/Structures/Exceptions/MissingCookieException.cs
+++ Pepper.Commons.Maimai/Structures/Exceptions/MissingCookieException.cs
@@ -2,7 +2,7 @@ namespace Pepper.Commons.Maimai.Structures.Exceptions
 {
     public class MissingCookieException : System.Exception, IFriendlyException
     {
-        public MissingCookieException() : base("No cookie was provided. Try logging in.") { }
-        public string FriendlyMessage => "No cookie was provided. Try logging in.";
+        public MissingCookieException() : base("No cookie was provided. Try logging in via `m!login` in DM.") { }
+        public string FriendlyMessage => "No cookie was provided. Try logging in via `m!login` in DM.";
     }
 }
