f3ca4bd3d4a8434b6683945f29837b7a536aafbd
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2023-01-02 21:07:44 +0700

    maimaiDX : Add reload command description

--- Pepper.Frontends.Maimai/Commands/Reload.cs
+++ Pepper.Frontends.Maimai/Commands/Reload.cs
@@ -4,6 +4,7 @@ using Pepper.Commons.Maimai;
 using Pepper.Commons.Structures.CommandAttributes.Metadata;
 using Pepper.Frontends.Maimai.Database.MaimaiDxNetCookieProviders;
 using Pepper.Frontends.Maimai.Services;
+using Qmmands;
 using Qmmands.Text;
 
 namespace Pepper.Frontends.Maimai.Commands
@@ -22,6 +23,7 @@ namespace Pepper.Frontends.Maimai.Commands
         }
 
         [TextCommand("reload")]
+        [Description("Flush cached song data & reload everything from database. Useful when editing stuff.")]
         public async Task<IDiscordCommandResult> Exec()
         {
             await Context.Message.AddReactionAsync(Hourglass);
