0e94832eb68bc93a1fa559031eac9cef040d6981
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2023-06-08 11:47:50 +0700

    osu! : Remove static logger in beatmap context provider

--- Pepper.Frontends.Osu/Services/BeatmapContextProviderService.cs
+++ Pepper.Frontends.Osu/Services/BeatmapContextProviderService.cs
@@ -6,12 +6,14 @@ namespace Pepper.Frontends.Osu.Services
     public class BeatmapContextProviderService : Service
     {
         private readonly Dictionary<string, int> cache = new();
-        private static readonly ILogger Log = Serilog.Log.Logger.ForContext<BeatmapContextProviderService>();
+        private readonly ILogger log;
+
+        public BeatmapContextProviderService(ILogger log) => this.log = log;
 
         public void SetBeatmap(string channelId, int beatmapId)
         {
             cache[channelId] = beatmapId;
-            Log.Debug("Setting beatmap of channel {0} to {1}", channelId, beatmapId);
+            log.Debug("Setting beatmap of channel {0} to {1}", channelId, beatmapId);
         }
 
         public int? GetBeatmap(string channelId) => cache.TryGetValue(channelId, out var beatmapId) ? beatmapId : null;
