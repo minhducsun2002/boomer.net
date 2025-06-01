9aa9a1526c6476b038dab274b043f7348cfbcc91
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2025-06-01 13:28:09 +0700

    General : Use built-in RNG for pick command

--- Pepper.Commons/Commands/General/Pick.cs
+++ Pepper.Commons/Commands/General/Pick.cs
@@ -2,53 +2,28 @@ using Disqord;
 using Disqord.Bot.Commands;
 using Qmmands;
 using Qmmands.Text;
+using Random = System.Random;
 
 namespace Pepper.Commons.Commands.General
 {
     public class Pick : GeneralCommand
     {
-        private readonly HttpClient httpClient;
-
-        public Pick(HttpClient httpClient)
-        {
-            this.httpClient = httpClient;
-        }
-
         [TextCommand("pick", "choose")]
         [Description("Helps you throw a dice.")]
-        public async Task<IDiscordCommandResult> Exec(
+        public IDiscordCommandResult Exec(
             [Remainder][Description("What to choose from? Separate by `/`s.")] string query = ""
         )
         {
-            var choices = query.Split("/").Where(_ => !string.IsNullOrWhiteSpace(_)).ToArray();
-            if (choices.Length == 0)
+            var choices = query.Split("/").Where(piece => !string.IsNullOrWhiteSpace(piece)).ToArray();
+            return choices.Length switch
             {
-                return Reply("You gave me no choice!");
-            }
-
-            if (choices.Length == 1)
-            {
-                return Reply("You provided only one choice. You probably don't need me to guess?");
-            }
-
-            var pick = await httpClient.GetStringAsync(
-                $"https://www.random.org/integers/?num=1&min=1&max={choices.Length}&col=1&base=10&format=plain&rnd=new"
-            );
-
-            if (int.TryParse(pick, out var result))
-            {
-                return Reply(new LocalEmbed
+                0 => Reply("You gave me no choice!"),
+                1 => Reply("You provided only one choice. `Choices / are / written / like / this`."),
+                _ => Reply(new LocalEmbed
                 {
-                    Title = "And my choice is...",
-                    Description = choices[result - 1]
-                });
-            }
-
-            return Reply(new LocalEmbed
-            {
-                Title = "And my choice is...",
-                Description = choices[0]
-            });
+                    Title = "And my choice is...", Description = choices[Random.Shared.Next(choices.Length)]
+                })
+            };
         }
     }
 }
